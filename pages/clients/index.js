df2e21e6c52c509b85481ec211d041d17a93b363
Michael Towson <MTowson@gmail.com>
2021-11-10 10:14:43 -0800

    add dynamic clients page links

--- pages/clients/index.js
+++ pages/clients/index.js
@@ -1,7 +1,21 @@
+import Link from "next/link";
+
 function ClientsIndex() {
+  const clients = [
+    {id: '1', name: 'Maximilian'},
+    {id: '2', name: 'Michael'},
+  ]
+
   return (
     <div>
       <h1>Default landing page for Clients</h1>
+      <ul>
+        {clients.map((client) => ( 
+          <li key={client.id}>
+            <Link href={`/clients/${client.id}`}>{client.name}</Link> 
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
