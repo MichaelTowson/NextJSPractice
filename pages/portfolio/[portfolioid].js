bc778652ba1c73318c559268fac6252a81c7313f
Michael Towson <MTowson@gmail.com>
2021-11-10 09:46:06 -0800

    add folders and subroutes for practice

---
+++ pages/portfolio/[portfolioid].js
@@ -0,0 +1,9 @@
+import { useRouter } from 'next/router' //special next hook that lets us grab the value in our filename
+
+function PortfolioProjectPage() {
+  const router = useRouter();
+
+  return <div>
+    <h1>This is the Portfolio Page for </h1>
+  </div>
+}
